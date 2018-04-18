07d9cab314dd64692f8a98cd1bcb208fe0e093d0
Chetan Chavan <Q4420@Corp.qualitykiosk.com>
2018-04-18 11:33:33 +0530

    third commit

--- src/com/sample/Demo.java
+++ src/com/sample/Demo.java
@@ -3,9 +3,8 @@ package com.sample;
 public class Demo {
 
 	public static void main(String[] args) {
-System.out.println("My first project:Github");
-System.out.println("hiiiiii");
-System.out.println("wecome github");
+System.out.println("My first project sample1");
+
 
 	}
 
